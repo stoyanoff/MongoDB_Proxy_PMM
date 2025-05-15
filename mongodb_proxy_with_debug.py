7eef8ed07aff91bec45219eb3fa10bc66a138235
Stoyan Stoyanov <s.stoyanoff@gmail.com>
2025-05-15 16:55:55 +0300

    Fixing UUID

--- mongodb_proxy_with_debug.py
+++ mongodb_proxy_with_debug.py
@@ -6,7 +6,7 @@ import time
 import threading
 import json
 import uuid
-from bson import BSON, ObjectId, Int64, STANDARD
+from bson import BSON, ObjectId, Int64
 from bson.binary import Binary, STANDARD, OLD_UUID_SUBTYPE
 
 # ----------------- Helper Functions -----------------
